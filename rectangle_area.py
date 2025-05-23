19b162865a0398f0e0531aa5c99bd165c391d94f
Emane-11 <alhachimi2000@gmail.com>
2025-05-23 19:38:13 +0100

    Update rectangle_area.py

--- rectangle_area.py
+++ rectangle_area.py
@@ -1,5 +1,5 @@
 length = 10
 width = 5
 area = length * width
-print(f'The area of the rectangle is {area}')
+print(f'The area of the rectangle is: {area}')
 # The area of the rectangle with length 10 and width 5 is 50
